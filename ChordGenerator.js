231a763725aa00428d9cce47e9f6590a38639419
Michael Fewkes <mdfewkes@gmail.com>
2021-12-08 01:44:00 -0700

    Basic Generator working

--- ChordGenerator.js
+++ ChordGenerator.js
@@ -32,50 +32,160 @@ const NoteName = {
 function Chord(root = 0, quality = Quality.Major) {
 	this.root = (root < 0 ? root + 12 : root) % 12;
 	this.quality = quality;
+
+	this.getBitmask = function() {
+		this.root = (this.root < 0 ? this.root + 12 : this.root) % 12;
+		var chordMask = "1";
+
+		//Fill mask with quality
+		switch (quality) {
+			case Quality.Major:
+				chordMask = "10001001";
+				break;
+			case Quality.Minor:
+				chordMask = "10010001";
+				break;
+			case Quality.Diminished:
+				chordMask = "1001001";
+				break;
+			case Quality.Augmented:
+				chordMask = "100010001";
+				break;
+		}
+		chordMask = chordMask.padEnd(12, "0");
+
+		//Rorate mask to root
+		const rotate = chordMask.length - (this.root % chordMask.length);
+		chordMask = chordMask.slice(rotate) + chordMask.slice(0, rotate);
+
+		return chordMask;
+	}
 }
 
 function Rule(rootMotion = 0, startingQuality = Quality.Major, endingQuality = Quality.Major) {
-	this.rootMotion = rootMotion;
+	this.rootMotion = (rootMotion < 0 ? rootMotion + 12 : rootMotion) % 12;
 	this.startingQuality = startingQuality;
 	this.endingQuality = endingQuality;
 }
 
+//I V vi iii IV I IV V
+var testRules5 = [
+	new Rule(7, Quality.Major, Quality.Major),
+	new Rule(2, Quality.Major, Quality.Minor),
+	new Rule(7, Quality.Minor, Quality.Minor),
+	new Rule(1, Quality.Minor, Quality.Major),
+	new Rule(-5, Quality.Major, Quality.Major),
+	new Rule(5, Quality.Major, Quality.Major),
+	new Rule(2, Quality.Major, Quality.Major),
+	new Rule(-7, Quality.Major, Quality.Major),
+];
+
+//I vi IV V
+var testRules4 = [
+	new Rule(-3, Quality.Major, Quality.Minor),
+	new Rule(-4, Quality.Minor, Quality.Major),
+	new Rule(2, Quality.Major, Quality.Major),
+	new Rule(-5, Quality.Major, Quality.Major),
+];
+
+//I IV V IV
+var testRules3 = [
+	new Rule(5, Quality.Major, Quality.Major),
+	new Rule(2, Quality.Major, Quality.Major),
+	new Rule(-2, Quality.Major, Quality.Major),
+	new Rule(-5, Quality.Major, Quality.Major),
+];
+
+//I V vi IV
+var testRules2 = [
+	new Rule(7, Quality.Major, Quality.Major),
+	new Rule(2, Quality.Major, Quality.Minor),
+	new Rule(-4, Quality.Minor, Quality.Major),
+	new Rule(-5, Quality.Major, Quality.Major),
+];
+
+//I IV V I
+var testRules1 = [
+	new Rule(5, Quality.Major, Quality.Major),
+	new Rule(2, Quality.Major, Quality.Major),
+];
+
 var testRules = [
 	new Rule(7, Quality.Major, Quality.Major),
-	new Rule(5, Quality.Major, Quality.Major),
+	new Rule(-7, Quality.Major, Quality.Major),
+	new Rule(7, Quality.Minor, Quality.Minor),
+	new Rule(-7, Quality.Minor, Quality.Minor),
+	new Rule(-7, Quality.Major, Quality.Minor),
+	new Rule(2, Quality.Major, Quality.Major),
+	new Rule(-2, Quality.Major, Quality.Major),
+	new Rule(2, Quality.Major, Quality.Major),
+	new Rule(-2, Quality.Major, Quality.Major),
+	new Rule(2, Quality.Major, Quality.Minor),
+	new Rule(-2, Quality.Minor, Quality.Major),
+	new Rule(-3, Quality.Major, Quality.Minor),
+	new Rule(3, Quality.Minor, Quality.Major),
+	new Rule(0, Quality.Major, Quality.Minor),
+	new Rule(0, Quality.Minor, Quality.Major),
+	new Rule(0, Quality.Major, Quality.Augmented),
+	new Rule(0, Quality.Augmented, Quality.Major),
+	new Rule(0, Quality.Minor, Quality.Diminished),
+	new Rule(0, Quality.Diminished, Quality.Minor),
+	new Rule(0, Quality.Minor, Quality.Augmented),
+	new Rule(0, Quality.Augmented, Quality.Minor),
+	new Rule(0, Quality.Major, Quality.Diminished),
+	new Rule(0, Quality.Diminished, Quality.Major),
+	new Rule(1, Quality.Diminished, Quality.Major),
+	new Rule(1, Quality.Diminished, Quality.Minor),
+	new Rule(4, Quality.Major, Quality.Diminished),
 ];
 
 function ChordGenerator() {
 	var openDomain = [];
 	for (var i = 0; i < 12; i++) {
 		for (var j = 0; j < 4; j++) {
-			openDomain.push({root: i, quality: j});
+			openDomain.push(new Chord(i, j));
 		}
 	}
 	//console.log(openDomain);
+	var loopProgression = false;
 	var progression = [];
-	var rules = [];
+	var rules = testRules;
 
-	this.generateProgressionOfLength = function(numberOfChords) {
-		var cells = [];
+	var failsafeRules = [
+		new Rule(0, Quality.Major, Quality.Major),
+		new Rule(0, Quality.Minor, Quality.Minor),
+		new Rule(0, Quality.Diminished, Quality.Diminished),
+		new Rule(0, Quality.Augmented, Quality.Augmented),
+	]
+
+	this.generateProgressionOfLength = function(numberOfChords, loop = false) {
+		var passCell = [];
 		for (var i = 0; i < numberOfChords; i++) {
-			cells.push(openDomain.slice());
+			passCell.push(0);
 		}
 
-		runGenerator(cells);
+		return this.generateProgressionFromTemplate(passCell, loop);
 	}
 
-	this.generateProgressionFromTemplate = function(chordProgression) {
+	this.generateProgressionFromTemplate = function(chordProgression, loop = false) {
+		loopProgression = loop;
+
 		var cells = [];
 		for (var i = 0; i < chordProgression.length; i++) {
-			if (chordProgression[i].root != undefined) {
+			if (chordProgression[i].root != undefined && chordProgression[i].quality != undefined) {
 				cells.push([chordProgression[i]]);
+			} else if (chordProgression[i].length != undefined) {
+				cells.push(chordProgression[i]);
 			} else {
-				cells.push(openDomain.slice());
+				cells.push(JSON.parse(JSON.stringify(openDomain)));
 			}
 		}
+		//console.log("generateProgressionFromTemplate");
+		//console.log(cells);
 
 		runGenerator(cells);
+		renderProgression();
+		return progression;
 	}
 
 	this.SetRules = function(newRules)  {
@@ -86,31 +196,146 @@ function ChordGenerator() {
 		rules.push(newRule);
 	}
 
-	function runGenerator(cells) {
-		progression = cells.slice();
-
-		while (!isCollapsed()) {
-			iterate();
+	this.AddRules = function(newRules)  {
+		for (var i = 0; i < newRules.length; i++) {
+			rules.push(newRules[i]);
 		}
 	}
 
+	function AddRules(newRules)  {
+		for (var i = 0; i < newRules.length; i++) {
+			rules.push(newRules[i]);
+		}
+	}
+
+	function runGenerator(cells, itaration = 5) {
+		console.log("runGenerator " + itaration);
+		if (itaration == 1) AddRules(failsafeRules);
+		if (itaration <= 0) return;
+
+		progression = JSON.parse(JSON.stringify(cells));
+		console.log(JSON.parse(JSON.stringify(progression)));
+		for (var i = 0; i < progression.length; i++) {
+			propagate(i);
+		}
+		console.log(JSON.parse(JSON.stringify(progression)));
+		while (!isCollapsed()) {
+			iterate();
+		}
+		if (isBroken()) runGenerator(cells, itaration-1);
+	}
+
 	function iterate() {
+		//console.log("iterate");
 		var coordIndex = getMinEntripy();
 		collaspeAt(coordIndex);
+		console.log(JSON.parse(JSON.stringify(progression)));
 		propagate(coordIndex);
 	}
 
-	//Todo https://www.youtube.com/watch?v=2SuvO4Gi7uY 12:05
+	//Todo
 	function propagate(index) {
+		var stack = [];
+		stack.push(index);
+
+		while (stack.length > 0) {
+			//console.log(stack);
+			var currentIndex = stack.pop();
+			//console.log("propagate " + index + " " + currentIndex);
+
+			//Forward
+			if (currentIndex+1 < progression.length) { //no loop
+				//if (progression[currentIndex+1].length <= 1) continue;
+				var changed = false;
+
+				//Loop over first chord
+				for (var i = progression[currentIndex+1].length-1; i >= 0; i--) {
+					var matches = 0;
+					//Loop over second chord
+					for (var j = progression[currentIndex].length-1; j >= 0; j--) {
+						//Loop over rules
+						for (var r = 0; r < rules.length; r++) {
+							if (rulePossible(progression[currentIndex][j], progression[currentIndex+1][i], rules[r])) {
+								matches++;
+							}
+						}
+					}
+					if (matches <= 0) {
+						//console.log("Cutting " + progression[currentIndex+1][i].root + ":" + progression[currentIndex+1][i].quality + " at " + matches + " matches");
+						progression[currentIndex+1].splice(i, 1);
+						changed = true;
+					} else {
+						//console.log("Keeping " + progression[currentIndex+1][i].root + ":" + progression[currentIndex+1][i].quality + " at " + matches + " matches");
+					}
+				}
+				if (changed) {
+					stack.push(currentIndex+1);
+				}
+			}
+
+			//Backward
+			if (currentIndex-1 >= 0) { //no loop
+				//if (progression[currentIndex-1].length <= 1) continue;
+				var changed = false;
+
+				//Loop over first chord
+				for (var i = progression[currentIndex-1].length-1; i >= 0; i--) {
+					var matches = 0;
+					//Loop over second chord
+					for (var j = progression[currentIndex].length-1; j >= 0; j--) {
+						//Loop over rules
+						for (var r = 0; r < rules.length; r++) {
+							if (rulePossible(progression[currentIndex-1][i], progression[currentIndex][j], rules[r])) {
+								matches++;
+							}
+						}
+					}
+					if (matches <= 0) {
+						//console.log("Cutting " + progression[currentIndex-1][i].root + ":" + progression[currentIndex-1][i].quality + " at " + matches + " matches");
+						progression[currentIndex-1].splice(i, 1);
+						changed = true;
+					} else {
+						//console.log("Keeping " + progression[currentIndex-1][i].root + ":" + progression[currentIndex-1][i].quality + " at " + matches + " matches");
+					}
+				}
+				if (changed) {
+					stack.push(currentIndex-1);
+				}
+			}
+
+
+			//console.log(JSON.parse(JSON.stringify(progression)));
+
+		}
 
 	}
 
-	//Todo
-	function collaspeAt(index) {
+	function renderProgression() {
+		for (var i = 0; i < progression.length; i++) {
+			progression[i] = progression[i][Math.floor(Math.random() * progression[i].length)];
+		}
+	}
 
+	function rulePossible(chord1, chord2, rule) {
+		var newRoot = (chord1.root + rule.rootMotion < 0 ? chord1.root + rule.rootMotion + 12 : chord1.root + rule.rootMotion) % 12;
+		//console.log(chord1.root + ":" + chord1.quality + " " + chord2.root + ":" + chord2.quality + " " + rule.startingQuality + ":" + rule.endingQuality + ":" + rule.rootMotion)
+		return (chord1.quality == rule.startingQuality) 
+		  && (chord2.quality == rule.endingQuality) 
+		  && (newRoot == chord2.root);
+	}
+
+	function collaspeAt(index) {
+		//console.log(index);
+		progression[index] = [progression[index][Math.floor(Math.random() * progression[index].length)]];
+	}
+
+	function isCollapsedAt(index) {
+		if (progression[index].length > 1) return false;
+		else return true;
 	}
 
 	function isCollapsed() {
+		//console.log("Testing collapse")
 		for (var i = 0; i < progression.length; i++) {
 			if (progression[i].length > 1) return false;
 		}
@@ -124,9 +349,22 @@ function ChordGenerator() {
 		return false;
 	}
 
-	//Todo
 	function getMinEntripy() {
-		return 0;
+		var minis = [0];
+		var miniEnt = 10000000;
+
+		for (var i = 0; i < progression.length; i++) {
+			if (progression[i].length == miniEnt) {
+				minis.push(i);
+			} else if (progression[i].length < miniEnt && progression[i].length > 1) {
+				miniEnt = progression[i].length;
+				minis = [];
+				minis.push(i);
+			}
+		}
+
+		console.log(minis);
+		return minis[Math.floor(Math.random() * minis.length)];
 	}
 
 }
